ec40bfe4cb127bb3e2d5f8dad9c85215a39d1484
goudoulf <camille.assie@gmail.com>
2025-03-05 23:47:57 +0100

    feat: change colorscheme

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
-			require'nvim-treesitter.configs'.setup {
+			require("nvim-treesitter.configs").setup({
 				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
 				auto_install = true,
@@ -19,8 +19,7 @@ return {
 					end,
 					additional_vim_regex_highlighting = false,
 				},
-			}
+			})
 		end,
-
-	}
+	},
 }
